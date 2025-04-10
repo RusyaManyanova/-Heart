ba3277396146c807877bf3795ad6c2c86ef38212
RusyaManyanova <manynov4@gmail.com>
2025-04-10 23:55:26 +0700

    Add files via upload

---
+++ untitled-html (2)/script (2).js
@@ -0,0 +1,9 @@
+function goToNextPage() {
+    // Переход на следующую страницу
+    window.location.href = "../untitled-html (4)/index (4).html";
+}
+
+function goToNextPage1() {
+    // Переход на следующую страницу
+    window.location.href = "../untitled-html (3)/index (3).html";
+}
